2b5318c18d699b1617385310b8444e6402da1073
78346593+chubatovatiger <78346593+chubatovatiger@TeamCity>
2021-11-26 09:33:11 +0100

    TeamCity change in 'charts' project: New custom chart added to buildtype-graphs

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,8 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.CustomChart
+import jetbrains.buildServer.configs.kotlin.v2019_2.CustomChart.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildTypeCustomChart
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -28,6 +31,20 @@ version = "2021.2"
 project {
 
     buildType(Conf)
+
+    features {
+        buildTypeCustomChart {
+            id = "PROJECT_EXT_46"
+            title = "New chart title"
+            seriesTitle = "Serie"
+            format = CustomChart.Format.TEXT
+            series = listOf(
+                Serie(title = """Build Step #1 - Command Line "command line"""", key = SeriesKey.buildStepDuration("RUNNER_1")),
+                Serie(title = """Build Step #2 - Command Line "cmd"""", key = SeriesKey.buildStepDuration("RUNNER_2")),
+                Serie(title = """Build Step #3 - Command Line "мой шаг шаг"""", key = SeriesKey.buildStepDuration("RUNNER_3"))
+            )
+        }
+    }
 }
 
 object Conf : BuildType({
